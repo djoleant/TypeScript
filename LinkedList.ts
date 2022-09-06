789ebbef23b191d8977b61d5135c9fcbf0c76a59
Djordje Antic <48065134+djoleant@users.noreply.github.com>
2022-09-06 20:20:55 +0200

    Create LinkedList.ts

---
+++ LinkedList.ts
@@ -0,0 +1,11 @@
+//DEFINITION OF LINKED LIST in TS
+
+
+class ListNode {
+     val: number
+     next: ListNode | null
+     constructor(val?: number, next?: ListNode | null) {
+         this.val = (val===undefined ? 0 : val)
+         this.next = (next===undefined ? null : next)
+     }
+}
