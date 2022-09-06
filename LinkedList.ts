97b3677107977a28b4390225724fff4f810f3083
Djordje Antic <48065134+djoleant@users.noreply.github.com>
2022-09-06 20:50:48 +0200

    Update LinkedList.ts

--- LinkedList.ts
+++ LinkedList.ts
@@ -1,11 +1,39 @@
 //DEFINITION OF LINKED LIST in TS
 
 
-class ListNode {
-     val: number
-     next: ListNode | null
-     constructor(val?: number, next?: ListNode | null) {
-         this.val = (val===undefined ? 0 : val)
-         this.next = (next===undefined ? null : next)
+class ListNode<T> {
+     val: T;
+     next: ListNode<T> | null;
+     constructor(val?: number, next?: ListNode<T> | null) {
+         this.val = (val===undefined ? 0 : val);
+         this.next = (next===undefined ? null : next);
      }
 }
+
+class LinkedList<T> {
+     head: ListNode<T> | null;
+     
+     append(data: T): void {
+          if(!this.head) {
+               this.head = new ListNode(data);
+          } else {
+               let current = this.head;
+               while(current.next != null) {
+                    current = current.next;
+               }
+               current.next = new ListNode(data);
+          }
+     }
+     
+     search(data: T): ListNode<T> | null {
+          let current = this.head;
+          while (current) {
+               if (this.comparator(current.data, data)) {
+                    return current;
+               }
+          current = current.next;
+          }
+     return null;
+     }
+}
+
